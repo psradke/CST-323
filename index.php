343e7cfea261e88379806d0b1d6b2f0fd9f874b5
Phillip Radke <psradke@gmail.com>
2023-03-12 17:17:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Insert title here</title>
+</head>
+<frameset>
+    <frame>
+    <frame>
+    <noframes>
+    <body>
+    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    <?php
+    echo "Welcome to the home page."
+	?>
+    </body>
+    </noframes>
+</frameset>
+</html>
